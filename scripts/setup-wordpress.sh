8b54267b87fc19d7ffc2a9037664beb46bfa69cc
Ahmad-Ajm <ahaj82@hotmail.com>
2025-06-18 15:35:14 +0300

    .

--- scripts/setup-wordpress.sh
+++ scripts/setup-wordpress.sh
@@ -3,7 +3,7 @@ set -e
 sudo apt update && sudo apt install -y php php-mysql php-curl php-xml php-gd php-mbstring php-zip unzip curl wget git nginx mariadb-server
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar && sudo mv wp-cli.phar /usr/local/bin/wp
-sudo service mysql start
+sudo service mariadb start
 sudo mysql -e "CREATE DATABASE wp_ai_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 sudo mysql -e "CREATE USER 'wp_ai_user'@'localhost' IDENTIFIED BY 'wp_ai_pass';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON wp_ai_db.* TO 'wp_ai_user'@'localhost';"
